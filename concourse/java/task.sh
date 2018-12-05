26c21b57716ad99603623006bbbd8d01a346991d
Håkon Wang Schutt <hakon@schutt.no>
2018-12-05 22:04:53 +0100

    added move of package files for build

--- concourse/java/task.sh
+++ concourse/java/task.sh
@@ -9,4 +9,8 @@ export M2_LOCAL_REPO=".m2"
 
 
 mvn -f source/pom.xml install
+
+mv source/target/herokupipe-example-0.0.1-SNAPSHOT.jar jar-file/app.jar
+mv source/Dockerfile jar-file/Dockerfile
+
 echo -e "${GREEN}${CHECK} Maven install${NC}"
